180633c084a1d6d87c2f582e952c76293a35b0f1
Ryan <snorekr@gmail.com>
2021-11-01 17:03:55 -0400

    first commit / imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-
+import React, { useReducer } from 'react';
+import reducer, { initialState } from './reducers';
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className="App">
